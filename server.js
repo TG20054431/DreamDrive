8031c79bb3e55fb18db8dff28fe893589704099c
TG20054431 <20054431@studenti.uniupo.it>
2025-07-03 11:13:50 +0200

    modifica server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/utente', utenteRouter);
-app.use('/admin', adminRouter);
+app.use('/adminl', adminRouter);
 
 // Avvio del server
 app.listen(PORT, () => {
